6fa0e42830bb91b8aa99320957761d1a46feaad4
Mayumi Saito <160694270+bunyumi@users.noreply.github.com>
2024-04-13 12:09:59 +0900

    再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,11 @@
 const today = new Date();
 
-const year = today.getFullYear();
+const year = today.getFullYear(); 
 
-const month = today.getMonth() + 1;
+const month = today.getMonth() + 1; 
 
-const day = today.getDay() + 7;
+const day = today.getDate(); 
 
 console.log(year + '年' + month + '月' + day + '日');
+
+
