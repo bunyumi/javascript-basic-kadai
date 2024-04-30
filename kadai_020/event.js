b8296eb9fe374b2fe42af99de1c34305e58f4c78
Mayumi Saito <160694270+bunyumi@users.noreply.github.com>
2024-04-30 20:17:54 +0900

    再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,9 +2,6 @@ const text = document.querySelector("#text");
 
 const btn = document.querySelector("#btn");
 
-btn.addEventListener('click',()=>{
-  const textSecond = document.createElement('h2');
-  textSecond.textContent = 'ボタンをクリックしました';
-  document.body.appendChild(textSecond);
+btn.addEventListener('click', ()=>{
+  text.textContent    = "ボタンをクリックしました。";
 });
-
